d11eaab9cab8021ae7d96dce1e85dc8338025506
ozank <ozankoksal@hotmail.com>
2023-08-01 17:24:23 +0300

    Added partial composite implementation

---
+++ composite/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+type IComponent interface {
+	Draw()
+	Add(c *IComponent)
+	Remove() (*IComponent, error)
+	getChild(i int) (IComponent, error)
+}
+
+type Graphic struct {
+	name string
+}
+
+func (c Graphic) Draw() {
+	fmt.Println(c.name)
+}
+
+type (
+	Rectangle struct {
+		component Graphic
+	}
+	Line struct {
+		component Graphic
+	}
+	Text struct {
+		component Graphic
+	}
+)
+
+func (c Line) Draw() {
+	fmt.Println("Line")
+}
+
+func (c Text) Draw() {
+	fmt.Println("Text")
+}
+
+func (c Rectangle) Draw() {
+	fmt.Println("Rectangle")
+}
+
+type Picture struct {
+	component Graphic
+	children  []IComponent
+}
+
+func (c Picture) Draw() {
+	fmt.Println("picturestart")
+	for _, child := range c.children {
+		child.Draw()
+	}
+	fmt.Println("pictureend")
+}
+
+func main() {
+}
